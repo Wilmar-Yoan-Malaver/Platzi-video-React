e042d5341791b68b86a336b385e3eb8a69ac654c
Yoan Malaver <yoanmalaver@gmail.com>
2020-09-14 13:37:33 -0500

    Container : 404 page Not Found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFount = () => (
+  <section className='not_found'>
+    <div className='box hingebox'>
+      <p className='text animated pulse'>Error 404</p>
+      <p className='text '>Page not Found</p>
+    </div>
+  </section>
+);
+
+export default NotFount;
